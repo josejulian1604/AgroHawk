ff2bcaae6c85686a0034762972ebcd051bf11b80
KevinCordoba <kecordoba@ic-itcr.ac.cr>
2025-05-03 17:11:05 -0600

    Update routes.ts

--- agrohawk-frontend/app/routes.ts
+++ agrohawk-frontend/app/routes.ts
@@ -1,6 +1,12 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"),{
+export default [index("routes/home.tsx"),
+  {
     path: "login",
     file: "routes/login.tsx",
-  },] satisfies RouteConfig;
+  },
+  {
+    path: "forgot-password",
+    file: "routes/forgot-password.tsx"
+  },
+] satisfies RouteConfig;
