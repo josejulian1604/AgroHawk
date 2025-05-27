9c51d1904230abb3e7c42803244e3eb7a430bec6
KevinCordoba <kecordoba@ic-itcr.ac.cr>
2025-05-27 16:45:59 -0600

    add admin crud page

---
+++ agrohawk-frontend/app/admin/AdminCrud.tsx
@@ -0,0 +1,10 @@
+import AdminLayout from "../../components/AdminLayout";
+
+export default function AdminCrud() {
+  return (
+    <AdminLayout current="Administradores">
+      <h1 className="text-2xl font-bold mb-6 text-gray-800">Administradores</h1>
+     
+    </AdminLayout>
+  );
+}
